7553895aaa9c0337d9a58df9e9e20fa4bb7c34bf
sherryzhao1006 <sherryzhao1006@gmail.com>
2015-11-11 20:55:39 -0600

    Update LongestIncreasingSubsequence.java

--- src/dynamicprogramming/LongestIncreasingSubsequence.java
+++ src/dynamicprogramming/LongestIncreasingSubsequence.java
@@ -7,7 +7,22 @@ public class LongestIncreasingSubsequence {
 
 	}
 	public static int longestIncreasingSubsequence(int[] nums){
-		if(nums == null || nums.length )
+		if(nums == null || nums.length == 0){
+			return 0;
+		}
+		int max=0;
+		int n=nums.length;
+		int[] dp=new int[n];
+		for(int i=0;i<n;++i){
+			dp[i]=1;
+			for(int j=0;j<i;++j){
+				if(nums[j]<nums[i]){
+					dp[i]=Math.max(dp[i], dp[j]+1);
+				}
+			}
+			max=Math.max(max, dp[i]);
+		}
+		return max;
 	}
 
 }
