ace3fd4b82988f00747ce5c4330d689f2359bfef
sherryzhao1006 <sherryzhao1006@gmail.com>
2015-11-15 12:42:41 -0600

    Update FirstBadVersion.java

--- src/binarysearch/FirstBadVersion.java
+++ src/binarysearch/FirstBadVersion.java
@@ -6,29 +6,18 @@ Suppose you have n versions [1, 2, ..., n] and you want to find out the first ba
 
 You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
  */
-public class FirstBadVersion {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-	public int firstBadVersion(int n) {
-        if(n <=1){
-            return n;
-        }
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
         long left=1,right=n;
         while(left+1<right){
             long mid=left+(right-left)/2;
-            //if(isBadVersion((int)mid)){
-//                right=mid;
-//            }
-//            else{
-//                left=mid+1;
-//            }
+            if(isBadVersion((int)mid)){
+                right=mid;
+            }else{
+                left=mid+1;
+            }
         }
-//        if(isBadVersion((int)left)){
-//            return (int)left;
-//        }
-        return (int)right;
+        if(isBadVersion((int)left)) return (int)left;
+        else return (int)right;
     }
 }
