0d3b9cdc8fbb22f6747ab7220d1c4b191fddd627
sherryzhao1006 <sherryzhao1006@gmail.com>
2015-12-03 15:28:42 -0500

    Update MissingRanges.java

--- src/array/MissingRanges.java
+++ src/array/MissingRanges.java
@@ -3,43 +3,45 @@ import java.util.*;
 //Missing Ranges
 //Given a sorted integer array where the range of elements are [lower, upper] inclusive, return its missing ranges.
 //For example, given [0, 1, 3, 50, 75], lower = 0 and upper = 99, return ["2", "4->49", "51->74", "76->99"].
-public class MissingRanges {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-	public List<String> findMissingRanges(int[] nums, int lower, int upper) {
-		List<String> result=new ArrayList<String>();
+public class Solution {
+    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+        List<String> result=new ArrayList<String>();
         if(nums == null || nums.length == 0){
-            if(lower == upper) {
-                result.add(lower+"");
-            } else {
-                result.add(lower + "->" + upper);
+            if(upper==lower){
+                result.add(upper+"");
+            }else{
+                result.add(lower+"->"+upper);
             }
             return result;
         }
-        
-        int prev = lower;
-                
-        int curr = 0;
-        
-        while(curr < nums.length) {
-            if(nums[curr] - prev == 1) {
-                result.add(prev+"");
-            } else if (nums[curr] - prev >1) {
-                result.add(prev+"->"+(nums[curr]-1));    
+        if(nums[0] != lower){
+            if(nums[0]-1 == lower){
+                result.add(lower+"");
+            }else{
+                result.add(lower+"->"+(nums[0]-1));
             }
-            prev = nums[curr]+1;
-            curr++;
         }
-        if(nums[nums.length-1] != upper) {
-            if(nums[nums.length-1]+1 == upper) {
-                result.add(upper+"");
-            } else if( nums[nums.length-1]+1 < upper) {
-                result.add((nums[nums.length-1]+1) + "->" + upper);
+        int prev=nums[0];
+        for(int i=1;i<nums.length;++i){
+            while(i<nums.length && nums[i]==nums[i-1]+1){
+                i++;
             }
-        }        
+            if(i == nums.length) break;
+            int start=nums[i-1]+1;
+            if(nums[i]-start == 1){
+                result.add(start+"");
+            }else{
+                result.add(start+"->"+(nums[i]-1));
+            }
+        }
+        if(nums[nums.length-1] != upper){
+            if(nums[nums.length-1]+1 == upper){
+                result.add(upper+"");
+            }else{
+                result.add((nums[nums.length-1]+1)+"->"+upper);
+            }
+        }
         return result;
     }
 }
